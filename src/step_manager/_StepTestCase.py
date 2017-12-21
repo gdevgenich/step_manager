7e8e59cbbc24e7dafd0f03d4f9ec138227cedeb2
Vitold Sedyshev <vsedyshev@intermedia.net>
2017-12-21 15:47:53 +0000

    update
    git-svn-id: https://svn.intermedia.net/qa/dist/step_manager/trunk@12876 7770e91c-d54c-421e-8578-b3a092cf5dc9

--- src/step_manager/_StepTestCase.py
+++ src/step_manager/_StepTestCase.py
@@ -1,17 +1,36 @@
+#
+
+from __future__ import absolute_import
+
+from pbxut import PBXTestCase
+from abc import ABCMeta, abstractmethod
+
+from ._StepManager import StepManager
+
 
 class StepTestCase(PBXTestCase):
 
-    def assertFalse(self):
+    __metaclass__ = ABCMeta
+    __sm__ = None
 
-        err = AssertionError('')
-        err.content = self.content
 
-        raise err
+    def prepareReport(self, sm):
+        result = "Huh... This is report."
+        return result
 
+
+    def assertTrue(self, actual, msg=None):
+
+        if actual != True:
+            err = AssertionError(msg)
+            err.content = self.prepareReport(self.__sm__)
+
+    @abstractmethod
     def initialize(self, sm):
         raise NotImplementedError()
 
+
     def runTest(self):
-       sm = StepManager()
-       self.initialize(sm)
-       sm.run()
+       self.__sm__ = StepManager()
+       self.initialize(self.__sm__)
+       self.__sm__.run()
