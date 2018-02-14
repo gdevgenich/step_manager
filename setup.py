dfdccb3fb6bf2411255494fa0064eb64f5a54a69
Roman Sazanov <rsazanov@intermedia.net>
2018-02-14 10:40:06 +0000

    improve readability
    git-svn-id: https://svn.intermedia.net/qa/dist/step_manager/trunk@14147 7770e91c-d54c-421e-8578-b3a092cf5dc9

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author="Vitold Sedyshev, Dmitry Girdyuk",
     author_email="vit1251@gmail.com",
     keywords="test mnager",
-    version="1.21",
+    version="1.22",
     package_dir={'': 'src'},
     packages=find_packages('src'),
     zip_safe=False
